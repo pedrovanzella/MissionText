1d17b35f3ddba83d8c28c093d57709acd940d5ec
Pedro Vanzella <pedro@pedrovanzella.com>
2014-06-20 14:52:26 -0300

    Big volunteer button working

--- app/controllers/event_users_controller.rb
+++ app/controllers/event_users_controller.rb
@@ -3,6 +3,19 @@ class EventUsersController < ApplicationController
   
   # POST /eventusers
   def create
+    @event = Event.find(params.require(:event_id))
+    
+    unless @event.volunteers.include?(current_user)
+        @event.volunteers.push(current_user)
+    end
+    
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to @event, notice: "Successfully volunteered!"}
+      else
+        format.html { redirect_to @event, notice: "Failed to volunteer." }
+      end
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :events
+  resources :event_users
 
   devise_for :users
   get 'pages/home' => 'high_voltage/pages#show', id: 'home'
